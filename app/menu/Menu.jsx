f576ce69a974582458ef1fa42c04b4fdfd155abd
Leinah <gtpndn@gmail.com>
2024-05-30 10:42:20 +0900

    Menu xxl breakpoints added

--- app/menu/Menu.jsx
+++ app/menu/Menu.jsx
@@ -10,7 +10,8 @@ const cinzel_dec = Cinzel_Decorative({
 });
 
 const Menu = forwardRef((props, ref) => {
-  const twoxlAndXl = useMediaQuery({ minWidth: 1280 });
+  const xxl = useMediaQuery({ minWidth: 1536 });
+  const xl = useMediaQuery({ minWidth: 1280 });
   const large = useMediaQuery({ minWidth: 1024 });
   const medium = useMediaQuery({ minWidth: 768 });
   const small = useMediaQuery({ minWidth: 640 });
@@ -27,54 +28,63 @@ const Menu = forwardRef((props, ref) => {
   /* If Menu List is hovered */
   const [isHovered, setIsHovered] = useState(false);
 
-  useEffect(() => { 
-    if (twoxlAndXl) {
+  useEffect(() => {
+    if (xxl) {
       /* Animation */
-      setInitY({ 1: -100, 2: -100, 3: -115 });
-      setInitX({ 1: -325, 2: 400, 3: -200 });
-      setAnimateX({ 1: -350, 2: 425, 3: -225 });
+      setInitY({ 1: -100, 2: -100, 3: -200 });
+      setInitX({ 1: 75, 2: 975, 3: 175 });
+      setAnimateX({ 1: 50, 2: 1000, 3: 200 });
       /* Images */
-      setHeightImage({ 1: 300, 2: 200, 3: 300 });
-      setWidthImage({ 1: 300, 2: 200, 3: 300 });
+      setHeightImage({ 1: 250, 2: 250, 3: 250 });
+      setWidthImage({ 1: 250, 2: 250, 3: 250 });
+    } else if (xl) {
+      /* Animation */
+      setInitY({ 1: -100, 2: -100, 3: -200 });
+      setInitX({ 1: 25, 2: 775, 3: 50 });
+      setAnimateX({ 1: 0, 2: 800, 3: 75 });
+      /* Images */
+      setHeightImage({ 1: 250, 2: 250, 3: 250 });
+      setWidthImage({ 1: 250, 2: 250, 3: 250 });
     } else if (large) {
       /* Animation */
-      setInitY({ 1: -100, 2: -100, 3: -100 });
-      setInitX({ 1: -225, 2: 400, 3: -175 });
-      setAnimateX({ 1: -250, 2: 425, 3: -225 });
+      setInitY({ 1: -100, 2: -100, 3: -150 });
+      setInitX({ 1: 0, 2: 650, 3: 50 });
+      setAnimateX({ 1: -25, 2: 675, 3: 75 });
       /* Images */
-      setHeightImage({ 1: 200, 2: 150, 3: 200 });
-      setWidthImage({ 1: 200, 2: 150, 3: 200 });
+      setHeightImage({ 1: 200, 2: 200, 3: 200 });
+      setWidthImage({ 1: 200, 2: 200, 3: 200 });
     } else if (medium) {
       /* Animation */
-      setInitY({ 1: -100, 2: -100, 3: -75 });
-      setInitX({ 1: -210, 2: 300, 3: -150 });
-      setAnimateX({ 1: -235, 2: 325, 3: -175 });
+      setInitY({ 1: -100, 2: -100, 3: -125 });
+      setInitX({ 1: 0, 2: 450, 3: 65 });
+      setAnimateX({ 1: -25, 2: 475, 3: 35 });
       /* Images */
-      setHeightImage({ 1: 200, 2: 150, 3: 175 });
-      setWidthImage({ 1: 200, 2: 150, 3: 175 });
+      setHeightImage({ 1: 150, 2: 150, 3: 150 });
+      setWidthImage({ 1: 150, 2: 150, 3: 150 });
     } else if (small) {
       /* Animation */
-      setInitY({ 1: -50, 2: -50, 3: -50 });
-      setInitX({ 1: -150, 2: 250, 3: -95 });
-      setAnimateX({ 1: -175, 2: 260, 3: -115 });
+      setInitY({ 1: -50, 2: -50, 3: -100 });
+      setInitX({ 1: -25, 2: 375, 3: 25 });
+      setAnimateX({ 1: -50, 2: 400, 3: 0 });
       /* Images */
-      setHeightImage({ 1: 150, 2: 115, 3: 100 });
-      setWidthImage({ 1: 150, 2: 115, 3: 100 });
+      setHeightImage({ 1: 150, 2: 150, 3: 150 });
+      setWidthImage({ 1: 150, 2: 150, 3: 150 });
     } else if (xs) {
-      setInitY({ 1: -50, 2: -50, 3: -30 });
-      setInitX({ 1: -90, 2: 150, 3: -75 });
-      setAnimateX({ 1: -115, 2: 175, 3: -100 });
+      setInitY({ 1: -50, 2: -50, 3: -75 });
+      setInitX({ 1: -15, 2: 250, 3: 35 });
+      setAnimateX({ 1: -35, 2: 275, 3: 15 });
       /* Images */
-      setHeightImage({ 1: 100, 2: 75, 3: 80 });
-      setWidthImage({ 1: 100, 2: 75, 3: 80 });
+      setHeightImage({ 1: 100, 2: 100, 3: 100 });
+      setWidthImage({ 1: 100, 2: 100, 3: 100 });
     } else {
-      setInitY({ 1: -50, 2: -50, 3: -50 });
-      setInitX({ 1: -100, 2: 200, 3: -150 });
-      setAnimateX({ 1: -150, 2: 250, 3: -200 });
+      setInitY({ 1: -50, 2: -50, 3: -75 });
+      setInitX({ 1: -15, 2: 250, 3: 35 });
+      setAnimateX({ 1: -35, 2: 275, 3: 15 });
+      /* Images */
+      setHeightImage({ 1: 100, 2: 100, 3: 100 });
+      setWidthImage({ 1: 100, 2: 100, 3: 100 });
     }
-  }, [twoxlAndXl, large, medium, small, xs]);
-
-  
+  }, [xxl, xl, large, medium, small, xs]);
 
   return (
     <>
@@ -100,12 +110,15 @@ const Menu = forwardRef((props, ref) => {
           <div class="flex flex-col w-full text-primary">
             {MenuList.map((item) => (
               <div key={item.id}>
-                <div class="grid text-center 2xs:text-lg xs:text-xl sm:text-3xl md:text-4xl lg:text-5xl place-items-center">
-                  <span
+                <div class="collapse overflow-visible grid text-center 2xs:text-lg xs:text-xl sm:text-3xl md:text-4xl lg:text-5xl place-items-center">
+                  <input
+                    type="checkbox"
+                    name="my-accordion-1"
                     onMouseOver={() => setIsHovered(item.id)}
                     onMouseOut={() => setIsHovered(false)}
-                    className="cursor-pointer"
-                  >
+                    className="cursor-pointer w-6/12 grid place-items-start"
+                  />
+                  <span className="collapse-title ">
                     {item.title}
                     <AnimatePresence>
                       {isHovered === item.id && (
@@ -122,7 +135,7 @@ const Menu = forwardRef((props, ref) => {
                         >
                           <Image
                             src={item.image}
-                            className="mx-auto select-none absolute"
+                            className="absolute"
                             height={heiggtImage[item.id]}
                             width={widthImage[item.id]}
                           />
@@ -130,6 +143,9 @@ const Menu = forwardRef((props, ref) => {
                       )}
                     </AnimatePresence>
                   </span>
+                  <div className="collapse-content text-center w-full px-44">
+                    <p>hello</p>
+                  </div>
                 </div>
                 <hr className="border-[#b8926b] 2xs:my-0 sm:my-2 md:my-5 lg:my-8 lg:border lg:mx-20 xl:mx-44" />
               </div>
